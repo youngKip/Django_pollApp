0797c29e49067de8dbf8d834618ffc037f10e42c
Pawl <35077934+youngKip@users.noreply.github.com>
2020-08-28 23:53:48 +0300

    updated the tests.py to check for all possible bugs that might arise frmo question publication date.

--- polls/views.py
+++ polls/views.py
@@ -13,12 +13,11 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(
-        publication_date__lte=timezone.now()).order_by('-publication_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(publication_date__lte=timezone.now()).order_by('-publication_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
