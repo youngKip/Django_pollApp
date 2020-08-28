fb8f3116cb79944ba85839ea633857b4311fdc1e
Pawl <35077934+youngKip@users.noreply.github.com>
2020-08-28 23:28:29 +0300

    queryset function

--- polls/views.py
+++ polls/views.py
@@ -12,14 +12,13 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
-   def get_queryset(self):
+    def get_queryset(self):
     """
     Return the last five published questions (not including those set to be
     published in the future).
     """
     return Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-publication_date')[:5]
+        publication_date__lte=timezone.now()).order_by('-publication_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
